db9007bd03b167f9480dff0b176e5f3cfab11d0b
Nubar Heydarzada <heyderzadenubar@gmail.com>
2023-05-13 07:25:35 +0400

    First Console App

---
+++ Aprel/16/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salam. Xoş gəlmisiniz.");
+            Console.WriteLine("Adınızı daxil edin:");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
