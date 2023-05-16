a8fd1a3752797fb5a388519404432817515c28b8
Nubar Heydarzada <heyderzadenubar@gmail.com>
2023-05-16 14:03:09 +0400

    TaskInLesson completed

---
+++ Aprel/19/DataTypeTaskInLesson/DataTypeTaskInLesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeTaskInLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task in Lesson 1
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+            Console.WriteLine(c);
+            #endregion
+
+            #region Task in Lesson 1
+            string aze = "Azerbaijan";
+            string tur = "Turkey";
+            string azerTur = aze + " " + tur;
+            Console.WriteLine(azerTur);
+            Console.ReadLine();
+            #endregion  
+
+            char s = 'a';
+            Console.WriteLine(s);
+            Console.Read();
+
+
+        }
+    }
+}
