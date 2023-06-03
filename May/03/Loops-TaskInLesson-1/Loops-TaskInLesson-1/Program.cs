4d2d33694672fada1ce4180a574aefd042b398ab
Nubar Heydarzada <heyderzadenubar@gmail.com>
2023-06-03 11:02:45 +0400

    Loops TaskInLesson 1&2 completed

---
+++ May/03/Loops-TaskInLesson-1/Loops-TaskInLesson-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_TaskInLesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------Task in Lesson 1-----------------
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+            }
+
+            //------------Task in Lesson 2-----------------
+            for(int i = 0; i < 100; i++)
+            {
+                for (int j=0; j < 100; j++)
+                {
+                    Console.WriteLine(i);
+                    Console.WriteLine(j);
+                }
+            }
+        }
+    }
+}
