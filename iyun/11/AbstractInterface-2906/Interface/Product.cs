969d282822c754e1026052af638dfdacf3f32e8c
Nubar Heydarzada <heyderzadenubar@gmail.com>
2023-07-11 15:07:54 +0400

    interface half completed

--- iyun/11/AbstractInterface-2906/Interface/Product.cs
+++ iyun/11/AbstractInterface-2906/Interface/Product.cs
@@ -8,13 +8,12 @@ using System.Collections;
 namespace Interface
 {
    public class Product
-    {
+   {
         public int Id { get; set; }
         public string Brand { get; set; }
         public DateTime CreateDate { get; set; }
         public bool IsDeleted { get; set; }
         public DateTime? EditDate { get; set; }
         public DateTime? DeleteDate { get; set; }
-        
-    }
+   }
 }
