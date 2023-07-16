d0f9614969fb634f9ad4dc15b5e9d4d8500e54c1
Nubar Heydarzada <heyderzadenubar@gmail.com>
2023-07-17 01:47:42 +0400

    Tic Tac Toe game completed

---
+++ iyul/16/TicTacToe-Aprel07/TicTacToe-Aprel07/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TicTacToe_Aprel07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.UserLabel1 = new System.Windows.Forms.Label();
+            this.UserLabel2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Controls.Add(this.button6);
+            this.flowLayoutPanel1.Controls.Add(this.button7);
+            this.flowLayoutPanel1.Controls.Add(this.button8);
+            this.flowLayoutPanel1.Controls.Add(this.button9);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(249, 246);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(84, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(165, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(3, 84);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(84, 84);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(165, 84);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(3, 165);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(84, 165);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.TabIndex = 7;
+            this.button8.Tag = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // button9
+            // 
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(165, 165);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.TabIndex = 8;
+            this.button9.Tag = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.ButtonClick);
+            // 
+            // UserLabel1
+            // 
+            this.UserLabel1.AutoSize = true;
+            this.UserLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserLabel1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.UserLabel1.Location = new System.Drawing.Point(268, 22);
+            this.UserLabel1.Name = "UserLabel1";
+            this.UserLabel1.Size = new System.Drawing.Size(51, 17);
+            this.UserLabel1.TabIndex = 2;
+            this.UserLabel1.Text = "User1";
+            // 
+            // UserLabel2
+            // 
+            this.UserLabel2.AutoSize = true;
+            this.UserLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserLabel2.Location = new System.Drawing.Point(268, 39);
+            this.UserLabel2.Name = "UserLabel2";
+            this.UserLabel2.Size = new System.Drawing.Size(51, 17);
+            this.UserLabel2.TabIndex = 3;
+            this.UserLabel2.Text = "User2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 259);
+            this.Controls.Add(this.UserLabel2);
+            this.Controls.Add(this.UserLabel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label UserLabel1;
+        private System.Windows.Forms.Label UserLabel2;
+    }
+}
+
---
+++ iyul/16/TicTacToe-Aprel07/TicTacToe-Aprel07/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace TicTacToe_Aprel07
+{
+    public partial class Form1 : Form
+    {
+
+        bool isFirstUserClicked, isSecondUserClicked;
+        DialogResult dialogResult; //When User win
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (button.Tag != null)
+            {
+                if (isFirstUserClicked == false)
+                {
+                    isFirstUserClicked = true;
+                    isSecondUserClicked = false;
+                    button.Text = "X";
+                    button.Tag = null;
+                    button.ForeColor = Color.DarkGreen;
+                    button.Cursor = Cursors.Hand;
+                    UserLabel2.ForeColor = Color.OrangeRed;
+                    UserLabel1.ForeColor = Color.Black;
+                }
+                else if (isSecondUserClicked == false)
+                {
+                    isFirstUserClicked = false;
+                    isSecondUserClicked = true;
+                    button.Text = "O";
+                    button.Tag = null;
+                    button.ForeColor = Color.DarkRed;
+                    button.Cursor = Cursors.Hand;
+                    UserLabel1.ForeColor = Color.OrangeRed;
+                    UserLabel2.ForeColor = Color.Black;
+                }
+                if (button1.Text != "" || button2.Text != "" || button3.Text != "")
+                {
+                    if (button1.Text == button2.Text && button2.Text == button3.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (button4.Text != "" || button5.Text != "" || button6.Text != "")
+                {
+                    if (button4.Text == button5.Text && button5.Text == button6.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (button7.Text != "" || button8.Text != "" || button9.Text != "")
+                {
+                    if (button7.Text == button8.Text && button8.Text == button9.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (button1.Text != "" || button4.Text != "" || button7.Text != "")
+                {
+                    if (button1.Text == button4.Text && button4.Text == button7.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (button2.Text != "" || button5.Text != "" || button8.Text != "")
+                {
+                    if (button2.Text == button5.Text && button5.Text == button8.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (button3.Text != "" || button6.Text != "" || button9.Text != "")
+                {
+                    if (button3.Text == button6.Text && button6.Text == button9.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (button1.Text != "" || button5.Text != "" || button9.Text != "")
+                {
+                    if (button1.Text == button5.Text && button5.Text == button9.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (button3.Text != "" || button5.Text != "" || button7.Text != "")
+                {
+                    if (button3.Text == button5.Text && button5.Text == button7.Text)
+                    {
+                        if (isFirstUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User1 qazandi");
+                        }
+                        else if (isSecondUserClicked == true)
+                        {
+                            dialogResult = MessageBox.Show("User2 qazandi");
+                        }
+                    }
+                }
+                if (dialogResult == DialogResult.OK)
+                {
+                    button1.Text = "";
+                    button2.Text = "";
+                    button3.Text = "";
+                    button4.Text = "";
+                    button5.Text = "";
+                    button6.Text = "";
+                    button7.Text = "";
+                    button8.Text = "";
+                    button9.Text = "";
+                    button1.Tag = 1;
+                    button2.Tag = 2;
+                    button3.Tag = 3;
+                    button4.Tag = 4;
+                    button5.Tag = 5;
+                    button6.Tag = 6;
+                    button7.Tag = 7;
+                    button8.Tag = 8;
+                    button9.Tag = 9;
+                    isFirstUserClicked = false;
+                    isSecondUserClicked = false;
+                    dialogResult = DialogResult.None;
+                }
+                if (button1.Text != "" && button2.Text != "" && button3.Text != "" &&
+                    button4.Text != "" && button5.Text != "" && button6.Text != "" &&
+                    button7.Text != "" && button8.Text != "" && button9.Text != "" &&
+                    button1.Text != "" && button4.Text != "" && button7.Text != "" &&
+                    button2.Text != "" && button5.Text != "" && button8.Text != "" &&
+                    button3.Text != "" && button6.Text != "" && button9.Text != "" &&
+                    button1.Text != "" && button5.Text != "" && button9.Text != "" &&
+                    button3.Text != "" && button5.Text != "" && button7.Text != "")
+                {
+                    dialogResult = MessageBox.Show("Oyun heç-heçə bitdi");
+                }
+            }
+        }
+
+
+    }
+}
