3476ddb482adc27a2446a8ed8eded0cac82f0ee5
Nubar Heydarzada <heyderzadenubar@gmail.com>
2023-07-21 17:16:37 +0400

    Delegates homework completed

---
+++ iyul/21/DeligatesHomework-2504/DeligatesHomework-2504/Methods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeligatesHomework_2504
+{
+    public class Methods
+    {
+    public delegate void RunAnyMethod();
+
+        public static void Add()
+        {
+            Console.WriteLine("Add method");
+        }
+
+        public static void Edit()
+        {
+            Console.WriteLine("Edit method");
+        }
+
+        public static void Delete()
+        {
+            Console.WriteLine("Delete method");
+        }
+
+        public static void MyDelegate()
+        {
+            RunAnyMethod runAnyMethod = new RunAnyMethod(Add);
+            runAnyMethod += Edit;
+            runAnyMethod += Delete;
+            runAnyMethod.Invoke();
+        }
+
+    }
+}
