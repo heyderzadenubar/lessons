8324891fa9d9ee3f109ce96b633061d15e85e821
Nubar Heydarzada <heyderzadenubar@gmail.com>
2023-07-22 02:05:50 +0400

    Find odd and even numbers (with IEnumerable and IQueryable)

---
+++ iyul/21/DeligatesHomework-2504/Homework3/FindEvenOddNumbersClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public class FindEvenOddNumbersClass
+    {
+        public static void EvenNumberIEnumerable(int[] vs)   // yoxlamaq ucun yazdim 
+        {
+            IEnumerable<int> resultEvenNumbers = from a in vs
+                                                 where a%2==0
+                                                 select a;
+            foreach (int item in resultEvenNumbers)
+            {
+                Console.WriteLine(item + " ");
+            }
+        } 
+
+        public static void EvenNumberIQueryable(int[] vs)
+        {
+            IQueryable<int> result = vs.AsQueryable().Where(m => m % 2 == 0);
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void OddNumberIEnumerable(int[] vs) // yoxlamaq ucun yazdim 
+        {
+            IEnumerable<int> resultOddNumbers = from a in vs
+                                                where a > 400 && a <= 550 && a % 2 == 1
+                                                select a;
+
+            foreach (int item in resultOddNumbers)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+
+        public static void OddNumberIQueryable(int[] vs)
+        {
+            IQueryable<int> result = vs.AsQueryable().Where(m =>
+                                                            m % 2 == 1 &&
+                                                            m > 400 &&
+                                                            m < 550);
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ iyul/21/DeligatesHomework-2504/Homework3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+
+            #region Homework
+            /*Homework 3:
+             Bir dənə int array'ında 1-1000 arası rəqəmləriniz olsun (for ilə edin).
+             1. Array'dan cüt rəqəmləri sorğu ilə götürün.
+             2. Arrayda 400-dən böyük, 550-yə bərabər və ya kiçik olan, tək rəqəmləri götürün.*/
+            #endregion
+            int[] vs = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                vs[i] = i+1;
+            }
+
+
+            Console.WriteLine("------Even numbers--------");
+            FindEvenOddNumbersClass.EvenNumberIQueryable(vs);
+            Console.WriteLine("------Odd numbers--------");
+            FindEvenOddNumbersClass.OddNumberIQueryable(vs);
+        }
+       
+
+    }
+}
